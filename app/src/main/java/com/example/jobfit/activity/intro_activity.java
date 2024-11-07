0c0ed9a9d678d5a8a1a07de4b05c17fed82e9d34
Muhammad Ardiansyah Asrifah_H071221068 <ancakiky@gmail.com>
2024-11-08 06:09:43 +0800

    intro activity

--- app/src/main/java/com/example/jobfit/activity/intro_activity.java
+++ app/src/main/java/com/example/jobfit/activity/intro_activity.java
@@ -2,28 +2,26 @@ package com.example.jobfit.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-
-import com.example.jobfit.R;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Handler;
-import android.os.Looper;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.jobfit.R;
 
 public class intro_activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_intro); // Pastikan layout untuk intro_activity sesuai
 
-        new Handler(Looper.getMainLooper() ).postDelayed(new Runnable() {
+        // Menambahkan delay selama 2 detik (2000 ms) sebelum berpindah ke LoginActivity
+        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(intro_activity.this, Login.class));
-                finish();
+                // Pindah ke LoginActivity
+                Intent intent = new Intent(intro_activity.this, Login.class);
+                startActivity(intent);
+                finish(); // Menghentikan IntroActivity agar tidak kembali lagi ketika menekan tombol back
             }
-        },1000);
+        }, 2000); // 2000 ms = 2 detik
     }
 }
